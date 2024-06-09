cbf4fd604dda491ea8d856adfb9ca5d12e2c01e2
Vadim Shakhvorostov <thewadim76@yandex.ru>
2024-06-09 17:45:28 +0700

    Добавить контроллеры для фильмов и пользователей

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,78 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private static final LocalDate MINIMUM_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        validation(film);
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @GetMapping
+    public Collection<Film> getAllFilms() {
+        return films.values();
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film newFilm) {
+        if (!films.containsKey(newFilm.getId())) {
+            throw new NotFoundException("Фильм с id " + newFilm.getId() + " не найден");
+        }
+
+        Film oldFilm = films.get(newFilm.getId());
+
+        if (newFilm.getName() != null) {
+            oldFilm.setName(newFilm.getName());
+        }
+        if (newFilm.getDescription() != null) {
+            oldFilm.setDuration(newFilm.getDuration());
+        }
+        if (newFilm.getDescription() != null) {
+            oldFilm.setDescription(newFilm.getDescription());
+        }
+        if (newFilm.getReleaseDate() != null) {
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+        }
+        return oldFilm;
+    }
+
+    private void validation(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("Название не может быть пустым");
+        }
+        if (film.getDescription().length() > 200) {
+            throw new ValidationException("Описание не может содержать более 200 символов");
+        }
+        if (film.getReleaseDate().isBefore(MINIMUM_RELEASE_DATE)) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895");
+        }
+        if (film.getDuration() <= 0) {
+            throw new ValidationException("Продолжительность не может быть отрицательной");
+        }
+    }
+
+    private int getNextId() {
+        int currentMaxId = films.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,83 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        validation(user);
+        if (user.getName() == null || user.getName().isBlank()){
+            user.setName(user.getLogin());
+        }
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        if (!users.containsKey(newUser.getId())){
+            throw new NotFoundException("Пользователь с id " + newUser.getId() + "не найден");
+        }
+        User oldUser = users.get(newUser.getId());
+        if (newUser.getName() != null){
+            oldUser.setName(newUser.getName());
+        }
+        if (newUser.getLogin() != null){
+            oldUser.setLogin(newUser.getLogin());
+        }
+        if (newUser.getBirthday() != null){
+            oldUser.setBirthday(newUser.getBirthday());
+        }
+        if (newUser.getEmail() != null){
+            oldUser.setEmail(newUser.getEmail());
+        }
+        return oldUser;
+    }
+
+    private void validation(User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ValidationException("Имейл не может быть пустым");
+        }
+        if (!user.getEmail().contains("@")) {
+            throw new ValidationException("Имейл должен содержать @");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank()){
+            throw new ValidationException("Логин не может быть пустым");
+        }
+        if (user.getLogin().contains(" ")){
+            throw new ValidationException("Логин не может содержать пробелы");
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())){
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        }
+
+    }
+
+    private int getNextId() {
+        int currentMaxId = users.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+}
